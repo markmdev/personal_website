3ae183f576a4c31ea2431f7b4365e46b21b6ba2b
mark-mdev <markmorgan.dev@gmail.com>
2025-09-04 16:02:49 -0700

    Add sitemap integration, update site metadata, and enhance Google Analytics setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,14 @@ import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://markmdev.com/",
 
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
 });
